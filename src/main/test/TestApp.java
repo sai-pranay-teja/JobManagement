eb701c0974993dc2e3cef94773326b20564d085a
saipranayteja <pranayteja.ch@algonox.com>
2025-04-03 18:16:31 +0530

    dxv

---
+++ src/main/test/TestApp.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class TestApp {
+    @Test
+    public void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
