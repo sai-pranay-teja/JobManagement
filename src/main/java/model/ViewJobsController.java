27da7a6ffb1e995ae1509519ecf099b4a96ec612
saipranayteja <pranayteja.ch@algonox.com>
2025-04-07 20:06:09 +0530

    OKAY

--- src/main/java/model/ViewJobsController.java
+++ src/main/java/model/ViewJobsController.java
@@ -12,7 +12,7 @@ import java.util.Set;
 
 @WebServlet("/viewJobs")
 public class ViewJobsController extends HttpServlet {
-    private static final long serialVersionUID = 1L
+    private static final long serialVersionUID = 1L;
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
